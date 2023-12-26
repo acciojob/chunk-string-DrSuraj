6a7c9930496d4879bb1695524623d1a8f328c807
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 15:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function stringChop(str, size) {
-  // your code here
+function chunkString(str, chunkLength) {
+    // Check if the input string is null or empty
+    if (str === null || str === '') {
+        return [];
+    }
+
+    // Initialize an array to store the chunks
+    const chunks = [];
+
+    // Loop through the string and create chunks of the specified length
+    for (let i = 0; i < str.length; i += chunkLength) {
+        const chunk = str.substring(i, i + chunkLength);
+        chunks.push(chunk);
+    }
+
+    return chunks;
 }
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// Examples
+console.log(chunkString("Hello, world!", 5)); // ["Hello", ", wor", "ld!"]
+console.log(chunkString("12345", 2));         // ["12", "34", "5"]
+console.log(chunkString("abc", 5));           // ["abc"]
+console.log(chunkString(null, 3));            // []
+console.log(chunkString("", 4));              // []
